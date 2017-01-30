b11573a4fb8601c5da6fedb6dfcc62f1a991dce6
Max <agnusmaximus@berkeley.edu>
2017-01-29 19:57:40 -0800

    Use larger network

--- src/distributed_nn.cpp
+++ src/distributed_nn.cpp
@@ -26,11 +26,9 @@ int main(void) {
     int batch_size = 128;
     params->SetBatchsize(batch_size);
     params->AddLayer(batch_size, IMAGE_X*IMAGE_Y);
-    params->AddLayer(IMAGE_X*IMAGE_Y, 100);
-    //params->AddLayer(IMAGE_X*IMAGE_Y, 500);
-    //params->AddLayer(500, 800);
-    //params->AddLayer(800, 200);
-    //params->AddLayer(200, 100);
+    params->AddLayer(500, 800);
+    params->AddLayer(800, 200);
+    params->AddLayer(200, 100);
     params->AddLayer(100, N_CLASSES);
     params->SetLearningRate(1e-2);
 
